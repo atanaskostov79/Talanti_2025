cb666060ef57a54796bd12b21f2c5be2122661ca
atanaskostov79 <ankostov@gmail.com>
2026-03-14 10:15:34 +0200

    OOP

---
+++ oop_upr1.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def info(self):
+        print(f"Brand {self.brand}, Model {self.model}, Year {self.year}")
+
+
+cars = [Car("Toyota", "Corola", 1999), Car("Ford", "Mustang", 2000)]
+
+print(cars[0].year)
+
+for car in cars:
+    car.info()
